1bf13b05c725ad008bc27d00dbba1eda7f256719
Nick Erickson <neerick@amazon.com>
2020-11-02 17:53:25 -0800

    Updated to be aligned with mainline AG

--- autogluon_benchmark/utils/data_utils.py
+++ autogluon_benchmark/utils/data_utils.py
@@ -3,8 +3,8 @@ from io import StringIO
 import pandas as pd
 from pandas import DataFrame, Series
 
-from autogluon.tabular.utils.features.feature_metadata import FeatureMetadata
-from autogluon.tabular.utils.ml.utils import infer_problem_type
+from autogluon.tabular.features.feature_metadata import FeatureMetadata
+from autogluon.tabular.utils import infer_problem_type
 
 
 def get_data_metadata(X: DataFrame, y: Series) -> dict:
