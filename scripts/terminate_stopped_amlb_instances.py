0773ffb7c5f2a5a96c05b164ddb393b48e8ac254
Nick Erickson <neerick@amazon.com>
2021-10-29 13:02:21 -0700

    added EC2 termination script for stopped AMLB instances

---
+++ scripts/terminate_stopped_amlb_instances.py
@@ -0,0 +1,51 @@
+
+import time
+import boto3
+
+
+def terminate_stopped_ec2_amlb(region='us-east-1', dry_run=True):
+    """
+    Terminates all stopped EC2 instances from automlbenchmark in a given region.
+    This is used to cleanup the AWS account to avoid running into errors in future runs when many stopped instances exist.
+    Only run this if you know what you are doing. Set `dry_run=False` to actually terminate the instances.
+    """
+    ec2 = boto3.resource('ec2', region)
+    name_prefix = 'benchmark_aws.'
+
+    total_instance_count = 0
+    stopped_instance_count = 0
+    instances_to_terminate = []
+    for instance in ec2.instances.all():
+        total_instance_count += 1
+        if instance.state['Name'] == 'stopped':
+            stopped_instance_count += 1
+            for tag in instance.tags:
+                if tag['Key'] == 'Name':
+                    name = tag['Value']
+                    if name.startswith(name_prefix):
+                        print(f'Planning to terminate: {name}')
+                        instances_to_terminate.append(instance)
+                        break
+
+    terminating_instance_count = len(instances_to_terminate)
+    print(f'Total Instances        : {total_instance_count}')
+    print(f'Stopped Instances      : {stopped_instance_count}')
+    print(f'Stopped AMLB Instances : {instances_to_terminate}')
+    print(f'Terminating {terminating_instance_count} stopped AMLB instances in 20 seconds (kill process to abort!)')
+    if dry_run:
+        print('DRY RUN... INSTANCES WILL NOT BE TERMINATED')
+    time.sleep(20)
+    if dry_run:
+        print(f'Skipping termination because `dry_run==True`.')
+        return
+    print(f'Terminating...')
+    for i, instance in enumerate(instances_to_terminate):
+        instance.terminate()
+        print(f'Terminated {i + 1}/{terminating_instance_count} instances...')
+    print('Finished terminating instances')
+
+
+if __name__ == '__main__':
+    terminate_stopped_ec2_amlb()
+    # Uncomment below line to terminate instances
+    # terminate_stopped_ec2_amlb(dry_run=False)
