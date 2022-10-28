ddef5c7ef4cdded16cdd0692591d2953042edd93
innixma <innixma@gmail.com>
2022-10-28 23:03:48 +0000

    Update aggregate_zeroshot_metadata.py

--- scripts/aggregate_zeroshot_metadata.py
+++ scripts/aggregate_zeroshot_metadata.py
@@ -3,9 +3,9 @@ import argparse
 import pandas as pd
 
 from autogluon.common.loaders import load_pd, load_pkl
-from autogluon.common.loaders.load_s3 import list_bucket_prefix_suffix_s3, list_bucket_prefix_suffix_contains_s3
+from autogluon.common.loaders.load_s3 import list_bucket_prefix_suffix_contains_s3
 from autogluon.common.utils import s3_utils
-from autogluon.common.savers import save_pd, save_pkl
+from autogluon.common.savers import save_pkl
 
 
 def get_s3_paths(path_prefix: str, contains=None, suffix=None):
@@ -16,7 +16,9 @@ def get_s3_paths(path_prefix: str, contains=None, suffix=None):
 
 
 def aggregate_zeroshot_metadata(path_prefix: str, contains=None, invalid_datasets=None, folds=None):
-    paths_full = get_s3_paths(path_prefix, contains=contains, suffix='/zeroshot_metadata.pkl')
+    zeroshot_suffix = '/zeroshot_metadata.pkl'
+    results_suffix = 'scores/results.csv'
+    paths_full = get_s3_paths(path_prefix, contains=contains, suffix=zeroshot_suffix)
 
     if invalid_datasets is None:
         invalid_datasets = set()
@@ -37,9 +39,18 @@ def aggregate_zeroshot_metadata(path_prefix: str, contains=None, invalid_dataset
     aggregated_ground_truth = {}
     size_bytes_total = 0
     for i, path in enumerate(paths_valid):
+        path_to_scores = path.split('/output/', 1)[0] + '/output/' + results_suffix
+        scores = load_pd.load(path_to_scores)
+        task_name = scores['task'][0]
+        id = scores['id'][0]
+        fold_in_scores = scores['fold'][0]
+
         dataset_name, fold = path.split('zeroshot/')[1].split('/zeroshot_metadata.pkl')[0].rsplit('/', 1)
+        if task_name != dataset_name:
+            print(f'INFO: task name and AWS task name are not the same: {task_name}, {dataset_name}')
         fold = int(fold)
-        print(f'{i + 1}/{num_paths} | {dataset_name} | {fold} | {path}')
+        assert fold == fold_in_scores
+        print(f'{i + 1}/{num_paths} | {task_name} | {fold} | {path}')
         try:
             zeroshot_metadata = load_pkl.load(path)
         except Exception:
@@ -53,10 +64,10 @@ def aggregate_zeroshot_metadata(path_prefix: str, contains=None, invalid_dataset
             size_bytes_total += size_bytes
             print(f'TOT Size: {round(size_bytes_total / 1e6, 3)} MB')
 
-            if dataset_name not in aggregated_ground_truth:
-                aggregated_ground_truth[dataset_name] = {}
-            if fold not in aggregated_ground_truth[dataset_name]:
-                aggregated_ground_truth[dataset_name][fold] = {}
+            if task_name not in aggregated_ground_truth:
+                aggregated_ground_truth[task_name] = {}
+            if fold not in aggregated_ground_truth[task_name]:
+                aggregated_ground_truth[task_name][fold] = {}
                 for k in [
                     'y_val',
                     'y_test',
@@ -68,21 +79,27 @@ def aggregate_zeroshot_metadata(path_prefix: str, contains=None, invalid_dataset
                     'num_classes',
                     'label',
                 ]:
-                    aggregated_ground_truth[dataset_name][fold][k] = zeroshot_metadata[k]
-            if dataset_name not in aggregated_pred_proba:
-                aggregated_pred_proba[dataset_name] = {}
-            if fold not in aggregated_pred_proba[dataset_name]:
-                aggregated_pred_proba[dataset_name][fold] = {}
+                    aggregated_ground_truth[task_name][fold][k] = zeroshot_metadata[k]
+                aggregated_ground_truth[task_name][fold]['task'] = task_name
+                aggregated_ground_truth[task_name][fold]['id'] = id
+            if task_name not in aggregated_pred_proba:
+                aggregated_pred_proba[task_name] = {}
+            if fold not in aggregated_pred_proba[task_name]:
+                aggregated_pred_proba[task_name][fold] = {}
             for k in ['pred_proba_dict_val', 'pred_proba_dict_test']:
-                if k not in aggregated_pred_proba[dataset_name][fold]:
-                    aggregated_pred_proba[dataset_name][fold][k] = {}
+                if k not in aggregated_pred_proba[task_name][fold]:
+                    aggregated_pred_proba[task_name][fold][k] = {}
                 for m, pred_proba in zeroshot_metadata[k].items():
-                    aggregated_pred_proba[dataset_name][fold][k][m] = pred_proba
+                    aggregated_pred_proba[task_name][fold][k][m] = pred_proba
 
     return aggregated_pred_proba, aggregated_ground_truth
 
 
 def aggregate_zeroshot_from_params(s3_bucket, s3_prefix, version_name, constraint, invalid_datasets=None, folds=None):
+    assert version_name is not None
+    assert s3_bucket is not None
+    assert s3_prefix is not None
+    assert constraint is not None
     contains = f'.{constraint}.'
     result_path = f'{s3_prefix}{version_name}/'
 
@@ -93,8 +110,17 @@ def aggregate_zeroshot_from_params(s3_bucket, s3_prefix, version_name, constrain
         folds=folds,
     )
 
-    save_pkl.save(path=f's3://{s3_bucket}/aggregated/{result_path}zeroshot_pred_proba_{version_name}.pkl', object=aggregated_pred_proba)
-    save_pkl.save(path=f's3://{s3_bucket}/aggregated/{result_path}zeroshot_gt_{version_name}.pkl', object=aggregated_ground_truth)
+    if len(aggregated_pred_proba) == 0:
+        raise AssertionError('Empty Result!')
+
+    aggregated_pred_proba_path = f's3://{s3_bucket}/aggregated/{result_path}zeroshot_pred_proba_{version_name}.pkl'
+    aggregated_ground_truth_path = f's3://{s3_bucket}/aggregated/{result_path}zeroshot_gt_{version_name}.pkl'
+    print(f'Saving pred_proba output to {aggregated_pred_proba_path}')
+    print(f'Saving ground_truth output to {aggregated_ground_truth_path}')
+
+    save_pkl.save(path=aggregated_pred_proba_path, object=aggregated_pred_proba)
+    save_pkl.save(path=aggregated_ground_truth_path, object=aggregated_ground_truth)
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(formatter_class=argparse.RawTextHelpFormatter)
@@ -130,6 +156,10 @@ if __name__ == '__main__':
         s3_prefix=args.s3_prefix,
         version_name=args.version_name,
         constraint=args.constraint,
+        # s3_bucket=s3_bucket,
+        # s3_prefix=s3_prefix,
+        # version_name=version_name,
+        # constraint=constraint,
         invalid_datasets=invalid_datasets,
         folds=folds,
     )
