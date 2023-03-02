2a7b61ee37097a786388400b7e4c420b86186aaf
innixma <innixma@gmail.com>
2023-03-02 03:39:52 +0000

    Remove old aggregation script, rename new aggregation script

--- scripts/aggregate_openml_results.py
+++ scripts/aggregate_openml_results.py
@@ -1,38 +1,34 @@
 import argparse
 
-from autogluon.common.utils import s3_utils
-from autogluon.common.loaders import load_pd
-from autogluon.common.loaders.load_s3 import list_bucket_prefix_suffix_contains_s3
 from autogluon.common.savers import save_pd
 
-
-def aggregate(path_prefix: str, contains=None):
-    bucket, prefix = s3_utils.s3_path_to_bucket_prefix(path_prefix)
-    # objects = list_bucket_prefix_suffix_s3(bucket=bucket, prefix=prefix, suffix='scores/results.csv')
-    print(f'{bucket} | {prefix} | {contains}')
-    objects = list_bucket_prefix_suffix_contains_s3(bucket=bucket, prefix=prefix, suffix='scores/results.csv',
-                                                    contains=contains)
-    print(f'found {len(objects)} objects')
-    print('Printing first 3 objects:')
-    print(objects[:3])
-    paths_full = [s3_utils.s3_bucket_prefix_to_path(bucket=bucket, prefix=file, version='s3') for file in objects]
-    print(paths_full[:3])
-    df = load_pd.load(paths_full)
-    print(df)
-    return df
+from autogluon_benchmark import OutputSuiteContext
 
 
-def aggregate_from_params(s3_bucket, s3_prefix, version_name, constraint, aggregated_prefix='aggregated/'):
+def aggregate_results(s3_bucket,
+                      s3_prefix,
+                      version_name,
+                      constraint,
+                      include_infer_speed=False,
+                      mode='ray'):
     contains = f'.{constraint}.'
     result_path = f'{s3_prefix}{version_name}/'
+    path_prefix = f's3://{s3_bucket}/{result_path}'
+
     aggregated_results_name = f'results_automlbenchmark_{constraint}_{version_name}.csv'
 
-    df = aggregate(path_prefix=f's3://{s3_bucket}/{result_path}', contains=contains)
+    output_suite_context = OutputSuiteContext(
+        path=path_prefix,
+        contains=contains,
+        include_infer_speed=include_infer_speed,
+        mode=mode,
+    )
+    results_df = output_suite_context.aggregate_results()
+    print(results_df)
 
-    save_path = f's3://{s3_bucket}/{aggregated_prefix}{result_path}{aggregated_results_name}'
-
-    print(f'Saving to: {save_path}')
-    save_pd.save(path=save_path, df=df)
+    save_path = f's3://{s3_bucket}/aggregated/{result_path}{aggregated_results_name}'
+    save_pd.save(path=save_path, df=results_df)
+    print(f'Saved to {save_path}!')
 
 
 if __name__ == '__main__':
@@ -42,12 +38,20 @@ if __name__ == '__main__':
     parser.add_argument('--s3_prefix', type=str, help='Prefix for path to results needing aggregation', default='ec2/', nargs='?')
     parser.add_argument('--version_name', type=str, help='Root folder name in EC2 of results', nargs='?')
     parser.add_argument('--constraint', type=str, help='Name of constraint used in benchmark', default='1h8c', nargs='?')
-
+    parser.add_argument('--include_infer_speed', action='store_true')
+    parser.add_argument('--no-include_infer_speed', dest='include_infer_speed', action='store_false')
+    parser.add_argument('--mode', type=str, help='Whether to aggregate via "seq" or via "ray"', default='ray',
+                        nargs='?')
+    parser.set_defaults(keep_params=True)
+    parser.set_defaults(include_infer_speed=False)
+    parser.set_defaults(version_name="2023_02_20_bool_test")  # FIXME: Remove
     args = parser.parse_args()
 
-    aggregate_from_params(
+    aggregate_results(
         s3_bucket=args.s3_bucket,
         s3_prefix=args.s3_prefix,
         version_name=args.version_name,
         constraint=args.constraint,
+        include_infer_speed=args.include_infer_speed,
+        mode=args.mode,
     )
