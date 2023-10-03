deae199526a97cfde38e9d99f07c9daea4b2c8b9
innixma <innixma@gmail.com>
2023-10-03 17:21:44 +0000

    Update run_evaluation scripts

--- autogluon_benchmark/evaluation/runners/run_evaluation_openml_2023_gjibers.py
+++ autogluon_benchmark/evaluation/runners/run_evaluation_openml_2023_gjibers.py
@@ -5,37 +5,37 @@ from autogluon.bench.eval.evaluation.benchmark_evaluator import BenchmarkEvaluat
 
 
 if __name__ == '__main__':
-    framework_name_suffix = '_1h8c_gp3_amlb_2022'
+    framework_name_suffix = '_1h8c_gp3_amlb_2023'
     frameworks_run = [
         'AutoGluon_benchmark',
+        'AutoGluon_hq',
         'autosklearn',
-        # 'autosklearn2',
         'flaml',
-        'GAMA_benchmark',
         'H2OAutoML',
         'lightautoml',
-        'mljarsupervised_benchmark',
-        'TPOT',
-        'TunedRandomForest',
-        'RandomForest',
-        # 'mlr3automl',
+        'GAMA_benchmark',
+        # 'mljarsupervised_benchmark',
+        # 'mljarsupervised_perform',
+        # 'TPOT',
+        # 'TunedRandomForest',
+        # 'RandomForest',
+        # 'NaiveAutoML',
     ]
     frameworks_run = [f + framework_name_suffix for f in frameworks_run]
 
     paths = [
-        "s3://automl-benchmark-ag/aggregated/amlb/amlb_2022_preprocessed.csv",  # gjibers et al
+        "s3://automl-benchmark-ag/aggregated/amlb/amlb_2023_preprocessed.csv",
     ]
 
-    use_tid_as_dataset_name = False
     problem_types = ['binary', 'multiclass', 'regression']
+    use_tid_as_dataset_name = False
     treat_folds_as_datasets = False
-    # filter_errors = True
-    infer_batch_size = None
     task_metadata = "task_metadata.csv"
+    infer_batch_size = None
     evaluate(
         paths=paths,
         frameworks_run=frameworks_run,
-        output_suffix=f'2022_amlb_jmlr/1h8c/all',
+        output_suffix=f'2023_amlb_jmlr/1h8c/all',
         problem_type=problem_types,
         treat_folds_as_datasets=treat_folds_as_datasets,
         infer_batch_size=infer_batch_size,
@@ -46,8 +46,8 @@ if __name__ == '__main__':
     evaluate(
         paths=paths,
         frameworks_run=frameworks_run,
-        output_suffix=f'2022_amlb_jmlr/1h8c_fillna/all',
-        framework_nan_fill='constantpredictor_1h8c_gp3_amlb_2022',
+        output_suffix=f'2023_amlb_jmlr/1h8c_fillna/all',
+        framework_nan_fill='constantpredictor_1h8c_gp3_amlb_2023',
         problem_type=problem_types,
         treat_folds_as_datasets=treat_folds_as_datasets,
         infer_batch_size=infer_batch_size,
